e0736471faa83a7e9e0a8399e5420333c4a390cb
Susmitha_Nallagorla <susmithanallagorla2004@gmail.com>
2026-06-29 08:57:42 +0530

    Enhance data ingestion script for CSV files
    Add functionality to read and display CSV file details.

--- scripts/data_ingestion.py
+++ scripts/data_ingestion.py
@@ -3,3 +3,24 @@ import os
 data_folder = "data/raw"
 files = os.listdir(data_folder)
 print(files)
+for file in files:
+
+    if file.endswith(".csv"):
+
+        path = os.path.join(data_folder, file)
+
+        df = pd.read_csv(path)
+
+        print("="*60)
+        print(file)
+        print("="*60)
+
+        print("Shape")
+        print(df.shape)
+
+        print("\nData Types")
+        print(df.dtypes)
+
+        print("\nFirst Five Rows")
+        print(df.head())
+        print (df.isnull().sum())
